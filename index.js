c919caec66d11f0dc3f3af80768f099cdc0c3148
Osid ALkourd <alkordosid73@gmail.com>
2023-08-30 12:23:10 +0300

    define middleware for error handling

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ const PORT = process.env.PORT || 8000;
 const dbConnection = require('./config/db');
 const bodyParser = require('body-parser');
 const authRouter = require('./routes/authRouter');
-
+const {notFound , errorHandler} = require('./middleware/errorHandler')
 dbConnection();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// app.use('/' , (req , res) => {
-//     res.send('this is the first route');
-// });
 app.use('/api/user' , authRouter);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT , ()=>{
  console.log(`the server runing on ${PORT}`)
 });
